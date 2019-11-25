4661779085f27e5784043484a375961ab9aa5dbd
jambogambo <58192243+jambogambo@users.noreply.github.com>
2019-11-26 02:17:03 +0400

    Add files via upload

---
+++ precache-manifest.8b3536a157be3b7076b27acc0b2ef0b3.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd9bbc752a55fb5acc269c78569d2aaa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4a21d613af879e0997f9",
+    "url": "/static/css/main.8a517026.chunk.css"
+  },
+  {
+    "revision": "7092fec88ae307455f77",
+    "url": "/static/js/2.eecc48b4.chunk.js"
+  },
+  {
+    "revision": "4a21d613af879e0997f9",
+    "url": "/static/js/main.fcda77d5.chunk.js"
+  },
+  {
+    "revision": "d48a5f35fefc3a5ebeb4",
+    "url": "/static/js/runtime-main.a51ae23c.js"
+  },
+  {
+    "revision": "99b6fa5bf6122229f007b243ea6f2d76",
+    "url": "/static/media/banner-img.99b6fa5b.png"
+  },
+  {
+    "revision": "47ba2c2b2323daca5d5a249249685b34",
+    "url": "/static/media/movies1.47ba2c2b.jpg"
+  },
+  {
+    "revision": "7315a870c3da6bc2bbf19dfb491f4e33",
+    "url": "/static/media/movies2.7315a870.jpg"
+  },
+  {
+    "revision": "e9d048bc5e79b9d78842bd8735b09be6",
+    "url": "/static/media/movies3.e9d048bc.jpg"
+  },
+  {
+    "revision": "30928862d62ee1f54732777c00a92270",
+    "url": "/static/media/welcome-img.30928862.jpg"
+  }
+]);
